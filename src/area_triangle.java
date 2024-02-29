c396a3005c9a809778d893f294592b5746a8a683
princecorbett <princecorbett3@gmail.com>
2024-02-29 13:28:16 +0530

    Question Solved Till date

---
+++ src/area_triangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class area_triangle {
+    public static void main(String[] args)
+    {
+        float area;
+        float height;
+        float base;
+        System.out.println("Enter base and height of the triangle:  ");
+        Scanner sc = new Scanner(System.in);
+        height=sc.nextFloat();
+        base=sc.nextFloat();
+        area = 0.5f * height * base;
+        System.out.println("The area is :" + area);
+    }
+}
