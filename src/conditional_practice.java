d1dd1ec82cd0f8280d7395e9158c18814e8db01f
princecorbett <princecorbett3@gmail.com>
2024-03-01 23:20:19 +0530

    Done..

---
+++ src/conditional_practice.java
@@ -0,0 +1,20 @@
+public class conditional_practice {
+    public static void main(String[] args)
+    {
+        int x = 10;
+        int y = 20;
+
+        if (x == y)
+        {
+            System.out.println(x + " is equal to " + y);
+        }
+        else if (x > y)
+        {
+            System.out.println(x + " is greater than " + y);
+        }
+        else
+        {
+            System.out.println(x + " is smaller than " + y);
+        }
+    }
+}
